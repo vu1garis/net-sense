d91307f58abd1a32d6d3bc44a94df89bb96565a5
Don Whittle <don.whittle@autodesk.com>
2024-01-17 18:02:07 +0000

    refactored display and more character support

--- src/SenseHatLib/Implementation/SenseHatDisplay.cs
+++ src/SenseHatLib/Implementation/SenseHatDisplay.cs
@@ -23,83 +23,122 @@ internal sealed class SenseHatDisplay : ISenseHatDisplay, IDisposable
     
     public void Fill(ReadOnlySpan<Color> colors) => _client.Fill(colors);
 
-    public void DisplayText(
-        string text,
-        Color foreground,
-        Color background,
-        bool loop = false,
-        bool scroll = false,
-        int delay = 1000)
+    public void DisplayText(string text, Color foreground, Color background, bool loop = false, bool scroll = false, int delay = 1000)
     {
-        if (string.IsNullOrWhiteSpace(text))
-        {
-            throw new ArgumentException(nameof(text));
-        }
-
-        if (delay < 0)
-        {
-            throw new ArgumentException(nameof(delay));
-        }
+        ValidateParameters(text, delay);
 
         _textBuffer.Append(text, foreground, background);
-
         _textBuffer.IsLooping = loop;
 
-        ISenseHatFrame? current = null;
-
         _client.Clear();
+
+        ISenseHatFrame? current = null;
 
         while (true)
         {
             var next = _textBuffer.Next();
 
-            if (next == null && loop)
+            if (ShouldExitLoop(next, loop))
             {
-                throw new InvalidOperationException("Error, frame buffer empty and loop == true");
+                break;
             }
 
             if (scroll)
             {
-                if (current == null)
-                {
-                    // first character to display...
-                    current = next;
-
-                    _client.Fill(current.ToReadOnlySpan());
-
-                    Thread.Sleep(delay);
-                }
-                else
-                {
-                    // we need to slowly replace current with next, column by column
-                    // until next becomes current...
-                    for (int i = 1; i < SenseHatFrame.SENSEHAT_MAX_COLUMNS; i++)
-                    {
-                        var df = current
-                            .Select(rowFilter:.., columnFilter:i..)
-                            .AppendColumns(next.Select(rowFilter:.., columnFilter:..i));
-
-                        _client.Fill(df.ToReadOnlySpan());
-
-                        Thread.Sleep(delay);
-                    }
-
-                    current = next;
-                }
+#pragma warning disable CS8604 // Possible null reference argument, ShouldExitLoop asserts null 
+                current = DisplayScrollFrames(current, next);
+#pragma warning restore CS8604 // Possible null reference argument.
             }
             else
             {
-                // just display the character
-                _client.Fill(next.ToReadOnlySpan());
-
-                Thread.Sleep(delay);
+#pragma warning disable CS8604 // Possible null reference argument, ShouldExitLoop asserts null
+                DisplayFrame(next);
+#pragma warning restore CS8604 // Possible null reference argument.
             }
 
-            if (!loop)
+            Thread.Sleep(delay);
+        }
+    }
+
+    private void ValidateParameters(string text, int delay)
+    {
+        if (string.IsNullOrWhiteSpace(text))
+        {
+            throw new ArgumentException("Error invalid display text", nameof(text));
+        }
+
+        if (delay < 0)
+        {
+            throw new ArgumentException("Error, invalid delay expecting a millisecond value > 0", nameof(delay));
+        }
+    }
+
+    private bool ShouldExitLoop(ISenseHatFrame? next, bool loop)
+    {
+        if (next == null)
+        {
+            if (loop)
             {
-                break;
+                throw new InvalidOperationException("Error, frame buffer empty and loop == true");
+            }
+            else
+            {
+                // there are no more frames in the buffer and we are not 
+                // looping so we just need to exit
+                return true;
             }
         }
+
+        return false;
+    }
+
+    private ISenseHatFrame? DisplayScrollFrames(ISenseHatFrame? current, ISenseHatFrame next)
+    {
+        ISenseHatFrame? latestFrame = null;
+
+        if (current == null)
+        {
+            // current is used to keep track of the current frame.
+            // if we are in scrolling mode and the current frame is null
+            // it means we are about to display the first frame in which
+            // case we don't need to worry about scrolling and just
+            // set current to next, i.e. next is the first and current frame
+            latestFrame = next;
+
+            DisplayFrame(latestFrame);
+        }
+        else
+        {
+            // current is not null this means that it currently occupies the
+            // full display. We now we need to slowly replace current with next's
+            // pixels column by column until next becomes current.
+            latestFrame = ReplaceCurrentWithNext(current, next);
+        }
+
+        return latestFrame;
+    }
+
+    private ISenseHatFrame ReplaceCurrentWithNext(ISenseHatFrame current, ISenseHatFrame next)
+    {
+        for (int i = 1; i < SenseHatFrame.SENSEHAT_MAX_COLUMNS; i++)
+        {
+            var df = current
+                .Select(rowFilter:.., columnFilter:i..)
+                .AppendColumns(next.Select(rowFilter:.., columnFilter:..i));
+
+            DisplayFrame(df);
+        }
+
+        // once the for loop above has exited then the frame occupying
+        // the display will be equivalent to the frame next extracted from
+        // the buffer. return this frame (next) as the latest frame
+        return next;
+    }
+
+    private void DisplayFrame(ISenseHatFrame frame)
+    {
+        // just display the character
+        _client.Fill(frame.ToReadOnlySpan());    
     }
 
     #region IDisposable
